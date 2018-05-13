28297bb50137cc0b83c1f21f5ea7eb5689215b21
Jeri Veingrad <jeriveingrad@Jeris-Air.fios-router.home>
2018-05-12 20:21:30 -0400

    fixed redirect bug

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ var router = express.Router();              // get an instance of the express Ro
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
+    res.json({ message: 'hooray! welcome to our api!' });
 });
 
 // more routes for our API will happen here
@@ -96,6 +96,7 @@ viewRouter.route('/restaurants')
       if (err) {
         res.send(err);
       }
+        res.redirect(req.originalUrl);
     });
 });
 
